2c1245b490e78060df028bdd47495da1f474bbc8
Mark Johnson <wmdmark@gmail.com>
2014-01-06 22:04:59 -0500

    fixes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,327 @@
-(function(){"use strict";var t="undefined"!=typeof window?window:global;if("function"!=typeof t.require){var e={},r={},n=function(t,e){return{}.hasOwnProperty.call(t,e)},i=function(t,e){var r,n,i=[];r=/^\.\.?(\/|$)/.test(e)?[t,e].join("/").split("/"):e.split("/");for(var o=0,s=r.length;s>o;o++)n=r[o],".."===n?i.pop():"."!==n&&""!==n&&i.push(n);return i.join("/")},o=function(t){return t.split("/").slice(0,-1).join("/")},s=function(e){return function(r){var n=o(e),s=i(n,r);return t.require(s,e)}},a=function(t,e){var n={id:t,exports:{}};return r[t]=n,e(n.exports,s(t),n),n.exports},u=function(t,o){var s=i(t,".");if(null==o&&(o="/"),n(r,s))return r[s].exports;if(n(e,s))return a(s,e[s]);var u=i(s,"./index");if(n(r,u))return r[u].exports;if(n(e,u))return a(u,e[u]);throw Error('Cannot find module "'+t+'" from '+'"'+o+'"')},c=function(t,r){if("object"==typeof t)for(var i in t)n(t,i)&&(e[i]=t[i]);else e[t]=r},p=function(){var t=[];for(var r in e)n(e,r)&&t.push(r);return t};t.require=u,t.require.define=c,t.require.register=c,t.require.list=p,t.require.brunch=!0}})(),require.register("example",function(t,e){"The Rivets adaptor for Backbone models. \nSee docs on adaptors here: http://www.rivetsjs.com/docs/#adapters\nThis is a very simple Backbone adaptor. \nFor more advanced binding check out: \nhttps://github.com/azproduction/rivets-backbone-adapter";var r,n,i,o,s,a,u,c,p,h,l,d=function(t,e){return function(){return t.apply(e,arguments)}},f={}.hasOwnProperty,m=function(t,e){function r(){this.constructor=t}for(var n in e)f.call(e,n)&&(t[n]=e[n]);return r.prototype=e.prototype,t.prototype=new r,t.__super__=e.prototype,t};rivets.adapters[":"]={subscribe:function(t,e,r){return t.on("change:"+e,r)},unsubscribe:function(t,e,r){return t.off("change:"+e,r)},read:function(t,e){return t.get(e)},publish:function(t,e,r){return t.set(e,r)}},i=function(t){function e(){return this.hasLinks=d(this.hasLinks,this),u=e.__super__.constructor.apply(this,arguments)}return m(e,t),e.prototype.url="contacts/",e.prototype.defaults=function(){return{first_name:"",last_name:"",short_bio:"",github:"",twitter:"",website:""}},e.prototype.getGravatar=function(){return this.get("email")?"http://www.gravatar.com/avatar/"+hex_md5(this.get("email")):""},e.prototype.getFullName=function(){return""+this.get("first_name")+" "+this.get("last_name")},e.prototype.hasLinks=function(){var t;return t=_.uniq(_.values(this.get("links"))),!(1===t.length&&""===t[0])},e}(Backbone.Model),a=e("sample-data"),r=function(t){function e(){return c=e.__super__.constructor.apply(this,arguments)}return m(e,t),e.prototype.render=function(){return e.__super__.render.call(this),this.bindingView=rivets.bind(this.el,{model:this.model,view:this}),this},e.prototype.remove=function(){return this.bindingView.unbind(),e.__super__.remove.call(this)},e}(Backbone.View),n=function(t){function e(){return p=e.__super__.constructor.apply(this,arguments)}return m(e,t),e.prototype.el="#contact-form-view",e}(r),o=function(t){function e(){return h=e.__super__.constructor.apply(this,arguments)}return m(e,t),e.prototype.el="#contact-view",e}(r),s=function(t){function r(){return l=r.__super__.constructor.apply(this,arguments)}return m(r,t),r.prototype.el="#model-json-view",r.prototype.sampleData=e("sample-data"),r.prototype.events={"click a":function(t){var e;return a=e=$(t.currentTarget).data().sample,this.watched.clear({silent:!0}),this.watched.set(this.sampleData[e])}},r.prototype.initialize=function(t){return null==this.model&&(this.model=new Backbone.Model),this.watched=t.watch,this._setWatchedModelJSON(),this.listenTo(this.watched,"change",this._setWatchedModelJSON)},r.prototype._setWatchedModelJSON=function(){var t;return t=JSON.stringify(this.watched.toJSON(),null,"  "),this.model.set("modelJSON",t)},r}(r),rivets.formatters.linebreaksbr=function(t){return t.replace(/\n/g,"<br>")},rivets.binders["live-value"]={publishes:!0,bind:function(t){return $(t).on("keyup",this.publish)},unbind:function(t){return $(t).off("keyup",this.publish)},routine:function(t,e){return rivets.binders.value.routine(t,e)}},$(function(){return window.contactModel=new i,new n({model:contactModel}).render(),new o({model:contactModel}).render(),new s({watch:contactModel}).render()})}),require.register("sample-data",function(t,e,r){r.exports={michael:{first_name:"Michael",last_name:"Richards",short_bio:"Ruby/JavaScript developer. Author of Rivets.js.",email:"mike22e@gmail.com",twitter:"https://github.com/mikeric",github:"http://github.com/wmdmark",website:""},mark:{first_name:"Mark",last_name:"Johnson",short_bio:"Web designer, developer and teacher. Co-founder of Pathwright",email:"wmdmark@gmail.com",twitter:"http://twitter.com/wmdmark",github:"http://github.com/wmdmark",website:"http://pathwright.com"},mason:{first_name:"Mason",last_name:"Stewart",short_bio:"Frontender, JavaScripter, & Lisper",email:"mason@theironyard.com",twitter:"http://twitter.com/masondesu",github:"https://github.com/masondesu",website:""}}});
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+
+    if (has(cache, path)) return cache[path].exports;
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  var define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  var list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.register = define;
+  globals.require.list = list;
+  globals.require.brunch = true;
+})();
+require.register("example", function(exports, require, module) {
+"The Rivets adaptor for Backbone models. \nSee docs on adaptors here: http://www.rivetsjs.com/docs/#adapters\nThis is a very simple Backbone adaptor. \nFor more advanced binding check out: \nhttps://github.com/azproduction/rivets-backbone-adapter";
+var BoundView, ContactFormView, ContactModel, ContactView, ModelJSONView, sampleData, _ref, _ref1, _ref2, _ref3, _ref4,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+rivets.adapters[':'] = {
+  subscribe: function(obj, keypath, callback) {
+    return obj.on("change:" + keypath, callback);
+  },
+  unsubscribe: function(obj, keypath, callback) {
+    return obj.off("change:" + keypath, callback);
+  },
+  read: function(obj, keypath) {
+    return obj.get(keypath);
+  },
+  publish: function(obj, keypath, value) {
+    return obj.set(keypath, value);
+  }
+};
+
+"The Backbone Model and Views for our example.\nNormally these would be split into their own files but\nfor examples sake we're combinding everything here.";
+
+ContactModel = (function(_super) {
+  __extends(ContactModel, _super);
+
+  function ContactModel() {
+    this.hasLinks = __bind(this.hasLinks, this);
+    _ref = ContactModel.__super__.constructor.apply(this, arguments);
+    return _ref;
+  }
+
+  ContactModel.prototype.url = "contacts/";
+
+  ContactModel.prototype.defaults = function() {
+    return {
+      first_name: "",
+      last_name: "",
+      short_bio: "",
+      github: "",
+      twitter: "",
+      website: ""
+    };
+  };
+
+  ContactModel.prototype.getGravatar = function() {
+    if (!this.get("email")) {
+      return "";
+    }
+    return "http://www.gravatar.com/avatar/" + (hex_md5(this.get('email')));
+  };
+
+  ContactModel.prototype.getFullName = function() {
+    return "" + (this.get('first_name')) + " " + (this.get('last_name'));
+  };
+
+  ContactModel.prototype.hasLinks = function() {
+    var vs;
+    vs = _.uniq(_.values(this.get("links")));
+    return !(vs.length === 1 && vs[0] === "");
+  };
+
+  return ContactModel;
+
+})(Backbone.Model);
+
+sampleData = require('sample-data');
+
+BoundView = (function(_super) {
+  __extends(BoundView, _super);
+
+  function BoundView() {
+    _ref1 = BoundView.__super__.constructor.apply(this, arguments);
+    return _ref1;
+  }
+
+  BoundView.prototype.render = function() {
+    BoundView.__super__.render.call(this);
+    this.bindingView = rivets.bind(this.el, {
+      model: this.model,
+      view: this
+    });
+    return this;
+  };
+
+  BoundView.prototype.remove = function() {
+    this.bindingView.unbind();
+    return BoundView.__super__.remove.call(this);
+  };
+
+  return BoundView;
+
+})(Backbone.View);
+
+ContactFormView = (function(_super) {
+  __extends(ContactFormView, _super);
+
+  function ContactFormView() {
+    _ref2 = ContactFormView.__super__.constructor.apply(this, arguments);
+    return _ref2;
+  }
+
+  ContactFormView.prototype.el = "#contact-form-view";
+
+  return ContactFormView;
+
+})(BoundView);
+
+ContactView = (function(_super) {
+  __extends(ContactView, _super);
+
+  function ContactView() {
+    _ref3 = ContactView.__super__.constructor.apply(this, arguments);
+    return _ref3;
+  }
+
+  ContactView.prototype.el = "#contact-view";
+
+  return ContactView;
+
+})(BoundView);
+
+ModelJSONView = (function(_super) {
+  __extends(ModelJSONView, _super);
+
+  function ModelJSONView() {
+    _ref4 = ModelJSONView.__super__.constructor.apply(this, arguments);
+    return _ref4;
+  }
+
+  ModelJSONView.prototype.el = "#model-json-view";
+
+  ModelJSONView.prototype.sampleData = require("sample-data");
+
+  ModelJSONView.prototype.events = {
+    "click a": function(e) {
+      var sample;
+      sampleData = sample = $(e.currentTarget).data().sample;
+      this.watched.clear({
+        silent: true
+      });
+      return this.watched.set(this.sampleData[sample]);
+    }
+  };
+
+  ModelJSONView.prototype.initialize = function(options) {
+    if (this.model == null) {
+      this.model = new Backbone.Model();
+    }
+    this.watched = options.watch;
+    this._setWatchedModelJSON();
+    return this.listenTo(this.watched, "change", this._setWatchedModelJSON);
+  };
+
+  ModelJSONView.prototype._setWatchedModelJSON = function() {
+    var json;
+    json = JSON.stringify(this.watched.toJSON(), null, '  ');
+    return this.model.set("modelJSON", json);
+  };
+
+  return ModelJSONView;
+
+})(BoundView);
+
+"Custom Rivets formatter to replace text line breaks with <br>s\nSee formatter docs here: http://www.rivetsjs.com/docs/#formatters";
+
+rivets.formatters.linebreaksbr = function(value) {
+  return value.replace(/\n/g, '<br>');
+};
+
+"Custom Rivets binder to update value bindings \"live\" (onkeyup)\nBindings documentation here: http://www.rivetsjs.com/docs/#binders";
+
+rivets.binders['live-value'] = {
+  publishes: true,
+  bind: function(el) {
+    return $(el).on('keyup', this.publish);
+  },
+  unbind: function(el) {
+    return $(el).off('keyup', this.publish);
+  },
+  routine: function(el, value) {
+    return rivets.binders.value.routine(el, value);
+  }
+};
+
+$(function() {
+  window.contactModel = new ContactModel();
+  new ContactFormView({
+    model: contactModel
+  }).render();
+  new ContactView({
+    model: contactModel
+  }).render();
+  return new ModelJSONView({
+    watch: contactModel
+  }).render();
+});
+
+});
+
+;require.register("sample-data", function(exports, require, module) {
+module.exports = {
+  michael: {
+    first_name: "Michael",
+    last_name: "Richards",
+    short_bio: "Ruby/JavaScript developer. Author of Rivets.js.",
+    email: "mike22e@gmail.com",
+    twitter: "https://twitter.com/mikeric",
+    github: "http://github.com/mikeric",
+    website: ""
+  },
+  mark: {
+    first_name: "Mark",
+    last_name: "Johnson",
+    short_bio: "Web designer, developer and teacher. Co-founder of Pathwright",
+    email: "wmdmark@gmail.com",
+    twitter: "http://twitter.com/wmdmark",
+    github: "http://github.com/wmdmark",
+    website: "http://pathwright.com"
+  },
+  mason: {
+    first_name: "Mason",
+    last_name: "Stewart",
+    short_bio: "Frontender, JavaScripter, & Lisper",
+    email: "mason@theironyard.com",
+    twitter: "http://twitter.com/masondesu",
+    github: "https://github.com/masondesu",
+    website: ""
+  }
+};
+
+});
+
+;
